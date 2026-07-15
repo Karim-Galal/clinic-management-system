99a603fe385b324d6fbac32dc15c1ff269f7e06a
Karim-Galal <galalabo098@gmail.com>
2026-07-15 16:31:06 +0300

    sanctum & breeze api

---
+++ backend/routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use Http\Controllers\API\AuthController;
+
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+
+
